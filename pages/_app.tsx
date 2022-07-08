68b659e7c908bdd8864b8319dc094b5edcb5bf8a
Shlok D. Mange <shlok_mange@protonmail.com>
2022-07-08 14:33:14 +0530

    Tailwind + Next + Rainbowkit setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,47 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '@rainbow-me/rainbowkit/styles.css'
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit'
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps)  => {
+  
+  const { chains, provider } = configureChains(
+    [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+    [
+      alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
+      publicProvider()
+    ]
+  );
+  
+  const { connectors } = getDefaultWallets({
+    appName: 'Cryptify Engineering Technologies.',
+    chains
+  });
+  
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider
+  })
+
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
 }
 
 export default MyApp
